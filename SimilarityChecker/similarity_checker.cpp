d0e8d8243e205509bc9c7aba8ec2ebaee16737fa
taeung.yoon <ytw0201@gmail.com>
2024-07-04 08:48:23 +0900

    [refactoring] extract method

--- SimilarityChecker/similarity_checker.cpp
+++ SimilarityChecker/similarity_checker.cpp
@@ -5,16 +5,24 @@ class SimilarityChecker
 {
 public:
 	int checkLength(string str1, string str2) {
-		if (str1.size() == str2.size())
-			return 60;
-
 		int len1 = str1.size();
 		int len2 = str2.size();
-		int A = len1 > len2 ? len1: len2;
-		int B = len1 < len2 ? len1 : len2;
+		
+		if (len1 == len2)
+			return 60;
+
+		int A = getLongStrLen(len1, len2);
+		int B = getShortStrLen(len1, len2);
 		int gap = A - B;
-		double result = (1 - gap / B) * 60;
+		double result = static_cast<double>(1 - gap / B) * 60;
 		
 		return result;
+	}
+
+	int getLongStrLen(const int strLen1, const int strLen2) {
+		return strLen1 > strLen2 ? strLen1 : strLen2;
+	}
+	int getShortStrLen(const int strLen1, const int strLen2) {
+		return strLen1 < strLen2 ? strLen1 : strLen2;
 	}
 };
