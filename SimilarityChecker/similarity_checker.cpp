860b80b348ac1c89ca85139de3d6155bf45a0ea9
taeung.yoon <ytw0201@gmail.com>
2024-07-04 08:37:53 +0900

    [feature] add logic for zero score

--- SimilarityChecker/similarity_checker.cpp
+++ SimilarityChecker/similarity_checker.cpp
@@ -7,6 +7,14 @@ public:
 	int checkLength(string str1, string str2) {
 		if (str1.size() == str2.size())
 			return 60;
-		return 0;
+
+		int len1 = str1.size();
+		int len2 = str2.size();
+		int A = len1 > len2 ? len1: len2;
+		int B = len1 < len2 ? len1 : len2;
+		int gap = A - B;
+		double result = (1 - gap / B) * 60;
+		
+		return result;
 	}
 };
--- SimilarityChecker_test/similarity_checker_test.cpp
+++ SimilarityChecker_test/similarity_checker_test.cpp
@@ -4,11 +4,20 @@
 
 using namespace std;
 
-TEST(SimilarityChecker, checkLength) {
+TEST(SimilarityChecker, checkLength_SameLength) {
 	string str1 = "ABCDE";
 	string str2 = "ABCDE";
 	SimilarityChecker checker;
 	int ret = checker.checkLength(str1, str2);
 	
 	EXPECT_EQ(60, ret);
+}
+
+TEST(SimilarityChecker, checkLength_LongerThanOrEqaulTo2Times) {
+	string str1 = "A";
+	string str2 = "BB";
+	SimilarityChecker checker;
+	int ret = checker.checkLength(str1, str2);
+
+	EXPECT_EQ(0, ret);
 }
