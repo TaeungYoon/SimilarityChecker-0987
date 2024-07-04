cadbafb49a147046c2fb65b28f8e22b520cdf15b
taeung.yoon <ytw0201@gmail.com>
2024-07-04 09:09:57 +0900

    [refactoring] extract method getPartialScore

--- SimilarityChecker/similarity_checker.cpp
+++ SimilarityChecker/similarity_checker.cpp
@@ -4,25 +4,44 @@ using namespace std;
 class SimilarityChecker
 {
 public:
-	int checkLength(string str1, string str2) {
-		int len1 = str1.size();
-		int len2 = str2.size();
-		
-		if (len1 == len2)
-			return 60;
+	const int MAX_SCORE = 60;
+	const int MIN_SCORE = 0;
 
-		int A = getLongStrLen(len1, len2);
-		int B = getShortStrLen(len1, len2);
-		int gap = A - B;
-		double result = static_cast<double>(1 - gap / B) * 60;
-		
-		return result;
+	int checkLength(const int len1, const int len2) {
+		if (isLengthSame(len1, len2))
+			return MAX_SCORE;
+
+		if (isLengthDouble(len1, len2))
+			return MIN_SCORE;
+
+		return getPartialScore(len1, len2);
 	}
 
+private:
 	int getLongStrLen(const int strLen1, const int strLen2) {
 		return strLen1 > strLen2 ? strLen1 : strLen2;
 	}
+
 	int getShortStrLen(const int strLen1, const int strLen2) {
 		return strLen1 < strLen2 ? strLen1 : strLen2;
+	}
+
+	bool isLengthSame(const int len1, const int len2)
+	{
+		return len1 == len2;
+	}
+
+	bool isLengthDouble(const int len1, const int len2)
+	{
+		return len1 >= len2 * 2 || len2 >= len1 * 2;
+	}
+
+	int getPartialScore(const int len1, const int len2)
+	{
+		int A = getLongStrLen(len1, len2);
+		int B = getShortStrLen(len1, len2);
+		int Gap = A - B;
+
+		return 60 - (60 * (Gap) / B);
 	}
 };
--- SimilarityChecker_test/similarity_checker_test.cpp
+++ SimilarityChecker_test/similarity_checker_test.cpp
@@ -9,7 +9,7 @@ public:
 	SimilarityChecker checker;
 	int checkLength(string str1, string str2)
 	{
-		return checker.checkLength(str1, str2);
+		return checker.checkLength(str1.size(), str2.size());
 	}
 };
 
@@ -22,5 +22,17 @@ TEST_F(SimilarityCheckerFixture, checkLength_SameLength) {
 TEST_F(SimilarityCheckerFixture, checkLength_LongerThanOrEqaulTo2Times) {
 	int expected_score = 0;
 	int actual_score = checkLength("A", "BB");
+	EXPECT_EQ(expected_score, actual_score);
+}
+
+TEST_F(SimilarityCheckerFixture, checkLength_PartialScoreStr1Longer) {
+	int expected_score = 20;
+	int actual_score = checkLength("AAABB", "BAA");
+	EXPECT_EQ(expected_score, actual_score);
+}
+
+TEST_F(SimilarityCheckerFixture, checkLength_PartialScoreStr2Longer) {
+	int expected_score = 30;
+	int actual_score = checkLength("AA", "AAE");
 	EXPECT_EQ(expected_score, actual_score);
 }
